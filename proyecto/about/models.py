0a55b0c27b5bf00013f6347d890bf047107c2659
Thomas <thomaslinares32@gmail.com>
2022-01-24 01:27:58 -0300

    f

--- proyecto/about/models.py
+++ proyecto/about/models.py
@@ -5,7 +5,7 @@ class About(models.Model):
     name = models.CharField(max_length=30, verbose_name="Nombre")
     parrafo_1 = models.TextField(verbose_name="Primer Parrafo")
     parrafo_2 = models.TextField(verbose_name="Segundo Parrafo")
-    image = models.ImageField(verbose_name="Foto de Perfil", upload_to="Proyect")
+    image = models.ImageField(verbose_name="Foto de Perfil", upload_to="Project")
     created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de Creación")
     update = models.DateTimeField(auto_now=True, verbose_name="Fecha de Actualización")
 

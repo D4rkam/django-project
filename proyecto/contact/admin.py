399700b34c24588357bd904d1648082aca21cd77
D4rkam <65268769+D4rkam@users.noreply.github.com>
2022-01-27 03:05:03 -0300

    Finish

--- proyecto/contact/admin.py
+++ proyecto/contact/admin.py
@@ -1,10 +1,5 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from .models import Contact
 
 # Register your models here.
 admin.site.register(Contact)
-=======
-
-# Register your models here.
->>>>>>> 7aa53a6dd013dc37f354cf930508aa4e74b902c0

88c49857bdace135a02729dd3dcefe86bee52f12
D4rkam <65268769+D4rkam@users.noreply.github.com>
2022-01-27 03:06:56 -0300

    Finish

--- proyecto/contact/views.py
+++ proyecto/contact/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-<<<<<<< HEAD
 from .models import Contact
 
 # Create your views here.
@@ -8,9 +7,3 @@ def contact(request):
         'contacts':Contact.objects.all()
     }
     return render(request, 'contact/contact.html', contacts)
-=======
-
-# Create your views here.
-def contact(request):
-    return render(request, "contact/contact.html")
->>>>>>> 7aa53a6dd013dc37f354cf930508aa4e74b902c0

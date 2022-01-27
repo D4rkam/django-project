2903df665eb98fd610f6992ea872dc8b33809bf4
D4rkam <65268769+D4rkam@users.noreply.github.com>
2022-01-27 03:05:50 -0300

    Finish

--- proyecto/contact/urls.py
+++ proyecto/contact/urls.py
@@ -2,9 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
     path('', views.contact, name="contact")
-=======
-    path('contact/', views.contact, name='contact')
->>>>>>> 7aa53a6dd013dc37f354cf930508aa4e74b902c0
 ]

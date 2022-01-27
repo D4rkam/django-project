fa6a671ff7a516c98ea6ced25e38a40544eccd52
D4rkam <65268769+D4rkam@users.noreply.github.com>
2022-01-27 03:04:24 -0300

    finish

--- proyecto/contact/apps.py
+++ proyecto/contact/apps.py
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-from tabnanny import verbose
-=======
->>>>>>> 7aa53a6dd013dc37f354cf930508aa4e74b902c0
 from django.apps import AppConfig
-
 
 class ContactConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'

33c0a0120c3bde4948b8f8045235790389e1a0da
D4rkam <65268769+D4rkam@users.noreply.github.com>
2022-01-27 03:08:47 -0300

    Finish

--- proyecto/portfolio/models.py
+++ proyecto/portfolio/models.py
@@ -2,15 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model): #Con esta clase creamos los campos para agregar nuevos 'POST', y traducimos el menú
-<<<<<<< HEAD
     title = models.CharField(max_length=200, verbose_name="Titulo", null=False)
     description = models.TextField(verbose_name="Descripcion", null=False)
     image = models.ImageField(verbose_name="Imagen", upload_to="Project", null=False)
-=======
-    title = models.CharField(max_length=200, verbose_name="Titulo")
-    description = models.TextField(verbose_name="Descripcion")
-    image = models.ImageField(verbose_name="Imagen", upload_to="Project")
->>>>>>> 7aa53a6dd013dc37f354cf930508aa4e74b902c0
     link = models.URLField(null=True, blank=True, verbose_name="Direccion Web")
     created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
     update = models.DateTimeField(auto_now=True, verbose_name="Fecha de actualización")
